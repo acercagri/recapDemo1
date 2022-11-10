1f54d395f9055cd0b5127d340da55ec9bcd4e9a1
acercagri <cagri-acer@outlook.com>
2022-11-10 12:17:15 +0300

    recapDemo1

---
+++ src/recapDemo1/recapDemo1.java
@@ -0,0 +1,21 @@
+package recapDemo1;
+
+public class recapDemo1 {
+
+	public static void main(String[] args) {
+		int boy1 = 176;
+		int boy2 = 158;
+		int boy3 = 180;
+		int enUzunBoy = boy1;
+		
+		if(boy2>boy1) {
+			enUzunBoy = boy2;
+			}
+		if(boy3>boy1) {
+			enUzunBoy = boy3;
+		}
+		System.out.println("enUzunBoy" + enUzunBoy);
+
+	}
+
+}
